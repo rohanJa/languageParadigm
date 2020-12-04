718b9077c8812010915984eb469460762d9a9b6f
rohanJa <rohan.jain150300@gmail.com>
2020-12-04 14:54:32 +0530

    Hositing added

---
+++ hoisting.js
@@ -0,0 +1,23 @@
+console.log(getName);
+var x=10;
+var getName = () => {
+    console.log("Namaste JavaScript");
+}
+
+console.log(getName2);
+
+var getName2 = function () {
+    console.log("getName2 function is invoked.");
+}
+
+console.log(getName2);
+
+// var x = 7;
+
+// function getName() {
+//     console.log("Namaste Javascript");
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
