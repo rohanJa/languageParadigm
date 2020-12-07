8b0b92e3d81455ef256477c54981b8fce0c71731
rohanJa <rohan.jain150300@gmail.com>
2020-12-07 21:30:26 +0530

    Hiosting updated

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,12 @@
+
+
 console.log(getName);
 var x=10;
-var getName = () => {
+function getName()  {
     console.log("Namaste JavaScript");
 }
 
-console.log(getName2);
+console.log(getName);
 
 var getName2 = function () {
     console.log("getName2 function is invoked.");
@@ -12,12 +14,10 @@ var getName2 = function () {
 
 console.log(getName2);
 
-// var x = 7;
+/*
+    getName2() is 1st in memory allocation get allocated "undefined" so if we call the function before its intialization 
+    it will through an error that getName2() is not defined. But in getName() function first the function "getName()" get
+    allocated with whole function . So, if we access it before its intialization it will get invoked as in the memory allocation
+    state it will be allocated so it does not matter if we call before or after its intialization.
+*/
 
-// function getName() {
-//     console.log("Namaste Javascript");
-// }
-
-// getName();
-// console.log(x);
-// console.log(getName);
